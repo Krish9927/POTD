e3b9c1368be50728a737ae592c14623ec268ef0c
Krish9927 <krishna.2024ca050@mnnit.ac.in>
2025-01-12 17:47:13 +0530

    Update matrissearch2d.cpp

--- DAY-20/matrissearch2d.cpp
+++ DAY-20/matrissearch2d.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row=matrix.size(),col=matrix[0].size();
-        int s=0,e=(row*col)-1;
-        int mid=s+(e-s)/2;
-        while(s<=e){
-            if(target==matrix[mid/col][mid%col]){
-                return true;
-            }
-            else if(target>matrix[mid/col][mid%col]){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
-            mid=s+(e-s)/2;
-        }
-        return false;
+        int rowindex=0,colindex=col-1;
+         while(rowindex<row&&colindex>=0){
+          int element=matrix[rowindex][colindex];
+          if(element==target){
+            return true;
+          }
+          if(element>target){
+            colindex--;
+          }
+          else{
+            rowindex++;
+          }
+         }
+         return false;
     }
 };
